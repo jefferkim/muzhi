9c7d3d2386d349ddde4a10cabebeb7271b1b4488
jinjianfeng <jinjianfeng@outlook.com>
2012-12-19 11:53:40 +0800

    delete unuse file

--- js/router.js
+++ js/router.js
@@ -6,77 +6,92 @@ Muzhi.Router = Backbone.Router.extend({
 
     routes: {
         '': "index", //首页
-        '!list/:id/p:pageNo':"list", //列表
-        '!sold':"sold",//售罄
-        '!my':"my"//我的斗价
+        '!list/:id/p:pageNo': "list", //列表
+        '!sold/p:pageNo': "sold",//售罄
+        '!my/p:pageNo': "my"//我的斗价
     },
 
     initialize: function () {
         var self = this;
-        var slider =  new Swipe($('#J-slider')[0]);
-        var liTpl = '<% _.each(navItems, function(item) { %><li><a href="#!list/<%= item.id%>/p1"><%= item.name%></a></li><%});%>';
-        $.ajax({
-            url:"http://api.waptest.taobao.com/rest/api2.do?api=mtop.mz.getMzNav&type=jsonp&callback=?&v=1.0&data={}",
-            success:function(resp){
-                self.menu = resp.data.defaultData.navItems;
-                $("#J-catList").html(_.template(liTpl,{navItems:resp.data.defaultData.navItems}));
-            }
+        var slider = new Swipe($('#J-slider')[0]);
+        $(".J-filter").on("click",function(e){
+            e.preventDefault();
+            $("#J-catSel").show();
         });
 
+        $("#J-catList").on("click","a",function(e){
+            e.preventDefault();
+            var target = e.target;
+            $("#J-catSel").hide();
+            self.navigate($(target).attr("href"),{'trigger':true});
+        })
     },
+
+    _listRender: function (resp) {
+        if(!Muzhi.Util._checkLogin(resp)) return;
+
+        if(resp.data.success == "false"){
+            notification.flash("调用请求错误").show();
+            return ;
+        }
+        var data = resp.data.defaultData;
+        Muzhi.Goods.reset(data.mzPartList);
+        new Muzhi.goodlistView({
+            collection: Muzhi.Goods
+        }).render();
+        var pageNav = new PageNav({'id': '#J-pageNav', 'pageCount': Math.ceil(data.mzExtPart.totalCount / 12), 'objId': 'p'});
+    },
+
     //列表页
-    list: function (id,pageNo) {
-
-        console.log(id);
-
+    list: function (id, pageNo) {
 
         /*Muzhi.mtopH5.getApi(url.api, "1.0", url.data, {},function(resp){
-            console.log(resp);
-            var data = resp.data.defaultData;
-            Muzhi.Goods.reset(data.mzPartList);
-            new Muzhi.goodlistView({
-                collection:Muzhi.Goods
-            }).render();
-        });*/
-        console.log(this.menu);
-        var   data = {"b2c":"0","cc":id,"pre":"0","page":pageNo||1,"pagesize":"12","ext":"1"};
+         console.log(resp);
+         var data = resp.data.defaultData;
+         Muzhi.Goods.reset(data.mzPartList);
+         new Muzhi.goodlistView({
+         collection:Muzhi.Goods
+         }).render();
+         });*/
+
+        var self = this;
+        if (!Muzhi.menuList)
+            Muzhi.Util.getMenu();
+        else
+            Muzhi.Util.setCurrentMenu(id);
+
+        var data = {"b2c": "0", "cc": id, "pre": "0", "page": pageNo || 1, "pagesize": "12", "ext": "1"};
 
         $.ajax({
-            url:'http://api.waptest.taobao.com/rest/api2.do?api=mtop.mz.getMzList&v=1.0&type=jsonp&callback=?&data='+
+            url: 'http://api.waptest.taobao.com/rest/api2.do?api=mtop.mz.getMzList&v=1.0&type=jsonp&callback=?&data=' +
                 JSON.stringify(data),
-            success:function(resp){
-
-                var data = resp.data.defaultData;
-                Muzhi.Goods.reset(data.mzPartList);
-                new Muzhi.goodlistView({
-                    collection:Muzhi.Goods
-                }).render();
-
-
-
-                var pageNav = new PageNav({'id':'#J-pageNav', 'pageCount':Math.ceil(data.mzExtPart.totalCount/12), 'objId':'p'});
-
-
+            success: function (resp) {
+                self._listRender(resp);
             }
         })
-
-
-
-
-
     },
 
-    sold:function(){
-
-
+    sold: function (pageNo) {
+        var self = this;
+        $.ajax({
+            url: 'http://api.waptest.taobao.com/rest/api2.do?api=mtop.mz.getMzBarelyList&v=1.0&type=jsonp&callback=?&data=' +
+                JSON.stringify({"b2c": "0", "page": pageNo || 1, "pagesize": "12"}),
+            success: function (resp) {
+                self._listRender(resp);
+            }
+        })
     },
 
-    my:function(){
-
-
-
+    my: function (pageNo) {
+        var self = this;
+        $.ajax({
+            url: 'http://api.waptest.taobao.com/rest/api2.do?api=mtop.mz.getMyMzList&v=1.0&type=jsonp&callback=?&data=' +
+                JSON.stringify({"page": pageNo || 1, "pagesize": "12"}),
+            success: function (resp) {
+                self._listRender(resp);
+            }
+        })
     }
-
 
 });
 
